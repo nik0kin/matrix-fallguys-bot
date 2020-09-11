090c2e6ad55832898db22348b401127df63c6694
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2020-09-10 20:23:38 -0500

    add stop script + fix message facing

--- src/message-formatter.ts
+++ src/message-formatter.ts
@@ -2,9 +2,9 @@ import { ShopItem } from './fall-guys';
 
 // "Rare Costume Top: Hatchling - 4500K"
 export function getShopItemString(item: ShopItem) {
-  const topOrBottom = item.type === 'costume' ? capitalizeFirstLetter(item.topOrBottom!) : '';
+  const topOrBottom = item.type === 'costume' ? ' ' + capitalizeFirstLetter(item.topOrBottom!) : '';
   const cost = `${item.cost}${item.currency === 'kudos' ? 'K' : 'C'}`;
-  return `${capitalizeFirstLetter(item.rarity)} ${capitalizeFirstLetter(item.type)} ${topOrBottom}: ${item.name} - ${cost}`;
+  return `${capitalizeFirstLetter(item.rarity)} ${capitalizeFirstLetter(item.type)}${topOrBottom}: ${item.name} - ${cost}`;
 }
 
 function capitalizeFirstLetter(str: string) {
